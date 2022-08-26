d1b47db6eea5016bb92070536157c0ea119f1dd6
Khanhlam1186 <khanhyt2002@gmail.com>
2022-08-26 10:59:11 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+  todos : Todo[] = [];
+  newTodo:string;
+//hàm lưu công việc khi thêm vào
+  saveTodo(){
+
+    if(this.newTodo){
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = true
+      this.todos.push(todo);
+      this.newTodo = '';
+    }else{
+      alert('please Enter Todo');
+    }
+  }
+//hàm đánh dấu công việc đã hoàn thành
+  done(id:number){
+
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+//hàm xóa công việc
+  remove(id:number){
+
+    this.todos = this.todos.filter((v,i) => i !== id);
+  }
 }
